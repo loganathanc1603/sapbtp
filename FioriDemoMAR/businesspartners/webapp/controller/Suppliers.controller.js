3e9b6dbdf68eda05723b01ff1cc080f91f6ba6d7
Loganathan Chinnasamy <logu_c@live.com>
2023-04-21 10:50:15 +0000

    app router Config for On-premise & CAPM Service & user-api

--- FioriDemoMAR/businesspartners/webapp/controller/Suppliers.controller.js
+++ FioriDemoMAR/businesspartners/webapp/controller/Suppliers.controller.js
@@ -20,6 +20,20 @@ sap.ui.define([
                 var iCount = oEvent.getParameter("total");
                 var sTitle = this.getOwnerComponent().getModel("i18n").getResourceBundle().getText("listTitle", [iCount]);
                 this.LocalModel.setProperty("/ListTitle", sTitle);
+            },
+
+            onPress: function(){
+                var sUrl = "browser/abc2aa84-8193-48e8-9cea-97d397d39047/root";
+                $.ajax({
+                    url: sUrl,
+                    type: "GET",
+                    dataType: "json",
+                    success: function(res) {
+                        console.log(res);
+                    }, error: function(oError){
+                        console.log(oError);
+                    }
+                });
             }
         });
     });
