62a845295bfa156d7b1f197d62f60f634b2462fa
loganathanc1603 <loganathan.chinnasaamy@gmail.com>
2023-09-04 09:43:13 +0000

    Job Scheduler application update

--- app/index.js
+++ app/index.js
@@ -3,6 +3,15 @@ const app = express();
 
 const PORT = process.env.PORT || 4000;
 
+app.use(express.json());
+app.use(express.urlencoded({ encoded: false }));
+
+app.post("/btpjob", (oRequest, oResponse) => {
+    const { firstName, lastName } = oRequest.body;
+    const dNow = new Date();
+    oResponse.status(201).send(`Job Started at: ${dNow} Hello ${firstName} ${lastName} Welcome to BTP Job Scheduler Demo`);
+});
+
 app.get('/', function (req, res) {
     res.send('Hello World')
 });
