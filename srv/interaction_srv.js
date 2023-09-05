f0092a8ce6290227d861e7df68e1db24b2d01b3d
loganathanc1603 <loganathan.chinnasaamy@gmail.com>
2023-09-05 06:37:52 +0000

    Service layer implementation and test files added

--- srv/interaction_srv.js
+++ srv/interaction_srv.js
@@ -8,7 +8,38 @@ class CatalogService extends cds.ApplicationService {
 
         this.before('READ', 'Interactions_Header', async (req) => {
             const { maxID } = await SELECT.one`max(ID) as maxID`.from(Interactions_Header);
+
+            const headerData = await SELECT.distinct.from(Interactions_Header);
+            const firstRow = headerData[0],
+                iLimit = 3;
+
+            const query = SELECT.from(Interactions_Header)
+                .columns
+                `{ID, PARTNER, LOG_DATE, BPCOUNTRY_code}`
+                .where
+                `ID < ${iLimit}`
+                //.groupBy
+                //`PARTNER`
+                .orderBy
+                `LOG_DATE desc`;
+
+            const customHeaderdata = await query;
         });
+
+        this.after('READ', 'Interactions_Header', async (headerData) => {
+            const aHeaderData = Array.isArray(headerData) ? headerData : [headerData];
+            aHeaderData.forEach((oData) => { 
+                if(oData.ID === 1){
+                    oData.PARTNER = 'LOGANATHAN';
+                }
+            });
+        });
+
+        this.before('CREATE', 'Interactions_Header', async (req) => {
+            const { maxID } = await SELECT.one`max(ID) as maxID`.from(Interactions_Header);
+            req.data.ID = maxID;
+        });
+
         return super.init();
     }
 }
