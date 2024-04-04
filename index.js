68309607ed7e553ecba77c07e21ff4f398d37d56
loganathanc1603 <loganathan.chinnasaamy@gmail.com>
2024-04-04 20:31:02 +0530

    Movie Database NodeJS Application

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import swaggerUi from "swagger-ui-express";
+import bodyParser from "body-parser";
+import swaggerFile from "./swagger.json" assert { type: "json" };
+import moviesController from "./controller/movies.js";
+import reviewController from "./controller/reviews.js";
+import cors from "cors";
+
+const jsonParser = bodyParser.json();
+
+const app = express();
+app.use(cors());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.get("/", (req, res) => {
+  // #swagger.ignore = true
+  res.redirect("/api-docs");
+});
+
+app.use(jsonParser);
+
+app.use("/api/v1/movies", moviesController);
+app.use("/api/v1/movies", reviewController);
+
+app.get('/getCurrentDate', (req, res) => {
+  const currentTime = new Date().toLocaleString();
+  res.json(
+    {
+      CurrentTime: currentTime,
+      Timezone: Intl.DateTimeFormat().resolvedOptions().timeZone
+    }
+    );
+});
+
+const PORT = process.env.PORT ?? 3000;
+app.listen(PORT, () => {
+  console.log(`Movie Database listening in localhost:${PORT}`);
+});
